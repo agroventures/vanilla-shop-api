6cd4017578e08ce8559396951d375a45660d5001
Dilnuwan Weerasuriya <dilnuwanweerasuriya@gmail.com>
2026-01-01 14:33:49 +0530

    Update transporter.js

--- utils/transporter.js
+++ utils/transporter.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer"
 export const transporter = nodemailer.createTransport({
     service: "gmail",
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.GMAIL_ADDRESS,
         pass: process.env.GMAIL_APP_PASSWORD
