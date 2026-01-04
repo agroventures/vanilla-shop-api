8790228dc7c14cb466a481676c1302630c0452d2
dilnuwanweerasuriya <dilnuwanweerasuriya@gmail.com>
2026-01-04 19:34:58 +0530

    Revert "updates"
    This reverts commit d609b54d63dc86c2cd2c78ff5430e7f1f72b1dfd.

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,6 +1,5 @@
 import express from "express";
 import { getAdminData, login, register } from "../controllers/adminController.js";
-import { authenticate } from "../middleware/authenticate.js";
 
 const adminRouter = express.Router();
 
@@ -9,13 +8,5 @@ adminRouter.get("/", getAdminData);
 adminRouter.post("/login", login);
 
 adminRouter.post("/register", register);
-
-//new
-adminRouter.get("/dashboard", authenticate, (req, res) => {
-    res.status(200).json({
-        email: req.user.email,
-        role: req.user.role
-    });
-});
 
 export default adminRouter;
